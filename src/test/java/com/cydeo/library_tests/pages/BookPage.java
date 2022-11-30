ff6cee836dfcd8966f95668155aee15459aa3354
Kirich1971 <kchokler@gmail.com>
2022-11-30 15:58:00 -0500

    US 159, 160 done by KiRich

---
+++ src/test/java/com/cydeo/library_tests/pages/BookPage.java
@@ -0,0 +1,40 @@
+package com.cydeo.library_tests.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class BookPage extends BasePage {
+
+    @FindBy (xpath = "//a[.=' Add Book']")
+    public WebElement addBookButton;
+
+    @FindBy (xpath = "//input[@name='name']")
+    public WebElement inputBookName;
+
+    @FindBy (xpath = "//input[@name='isbn']")
+    public WebElement inputBookIsbn;
+
+    @FindBy (xpath = "//input[@name='year']")
+    public WebElement inputBookYear;
+
+    @FindBy (xpath = "//input[@name='author']")
+    public WebElement inputBookAuthor;
+
+    @FindBy (xpath = "//select[@id='book_group_id']")
+    public WebElement selectBookGroup;
+
+    @FindBy (xpath = "//button[@type='submit']")
+    public WebElement saveChangesButton;
+
+    @FindBy (xpath = "//input[@type='search']")
+    public WebElement inputSearch;
+
+    @FindBy (xpath = "//table[@id='tbl_books']/tbody/tr/td[3]")
+    public WebElement bookNameInTable;
+
+    @FindBy (xpath = "//table[@id='tbl_books']/tbody/tr/td[1]/a")
+    public List<WebElement> borrowBookButtons;
+
+}
--- src/test/java/com/cydeo/library_tests/runner/CukesRunner.java
+++ src/test/java/com/cydeo/library_tests/runner/CukesRunner.java
@@ -17,8 +17,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/library_tests/step_definitions",
         dryRun = false,
-        tags = "@wip",
-        publish = false
+        tags = "@wip_kirich",
+        publish = true
 )
 
 public class CukesRunner {
